01d51395e265ff44802d4da64ddfc8f2467581bc
N2N-B <rednan69@gmail.com>
2023-07-29 16:22:23 +0530

    Initial commit - I started building the ping pong game using python from scratch

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Screen
+
+BG_COLOR = "black"
+
+my_screen = Screen()
+my_screen.bgcolor(BG_COLOR)
+my_screen.setup(width=800,height=600)
+my_screen.title("Ping Pong Game")
+
+
+
+my_screen.exitonclick()
